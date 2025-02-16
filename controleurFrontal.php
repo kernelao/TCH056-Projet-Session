04ee4a5f9dec1eb75ceb6cc67af4baf6c7b3bba8
mirai12d <TonEmailGitHub>
2025-02-16 16:24:48 -0500

    version4

--- controleurFrontal.php
+++ controleurFrontal.php
@@ -1,25 +1,32 @@
 <?php
-    
-    //Le contrôleur frontal reçoit la requête avec un paramètre lui indiquant l’action à accomplir. 	
-    //Inclusion de la manufacture de controleur (qui importe déjà tous les contrôleurs)
-    include_once "controleurs/manufactureControleur.class.php";
-    
-    //Obtenir le bon contrôleur
-    //Si la requête contenant le paramètre action n'est pas renseignée
-    if (!isset($_GET['action'])) {
-        // on reste à la page d'accueil
-        $action = "accueil";
-    } else {
-        // Sinon, on récupère l’action indiquée à accomplir
-        $action = $_GET['action'];
-    }
+// Activer l'affichage des erreurs pour débogage (à désactiver en production)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-    //Créer une instance du contrôleur adapté 
-    $controleur = ManufactureControleur::creerControleur($action);
-    
-    //Exécuter l'action et obtenir le nom de la vue
-    $nomVue = $controleur->executerAction();
+// Inclusion de la manufacture de contrôleurs (qui importe déjà tous les contrôleurs)
+include_once "controleurs/manufactureControleur.class.php";
 
-    //Inclure la bonne vue
-    include_once("vues/" . $nomVue);
+// Vérifier si une action est définie dans l'URL
+$action = isset($_GET['action']) ? $_GET['action'] : "accueil";
+
+// Créer une instance du contrôleur correspondant
+$controleur = ManufactureControleur::creerControleur($action);
+
+// Vérifier si le contrôleur a été correctement instancié
+if (!$controleur) {
+    die("Erreur : Aucun contrôleur trouvé pour l'action '$action'");
+}
+
+// Exécuter l'action et obtenir le nom de la vue à charger
+$nomVue = $controleur->executerAction();
+
+// Vérifier si la vue existe avant de l'inclure
+$cheminVue = "vues/" . $nomVue;
+if (!file_exists($cheminVue)) {
+    die("Erreur : La vue '$nomVue' n'existe pas !");
+}
+
+// Inclure la bonne vue
+include_once($cheminVue);
 ?>
